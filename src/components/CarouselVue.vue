5a2d764a4542ceec5873f48d735a8fd7c3fa7737
CharlyG <carlo.gammarota@gmail.com>
2022-12-21 01:25:02 -0300

    start carousel

--- src/components/CarouselVue.vue
+++ src/components/CarouselVue.vue
@@ -1,32 +1,32 @@
 <template>
   <div >
     <!-- {{ relatedProducts }} -->
-    <VueSlickCarousel :arrows="true" v-bind="settings">
-       
-            
-            <div v-for="item in relatedProducts" :key="item.id" >
-                
-                {{item.name}}
-            </div> 
-            <!-- <div>1</div>
-            <div>5</div>
-            <div>2</div>
-            <div>3</div> -->
-     <!-- <div v-for="item in relatedProducts" :key="item.id" >
-        <Card :product="item" class="CardStyles"></Card> 
-    </div>  -->
-    
-    </VueSlickCarousel>
+    <vue-slick-carousel
+          v-bind="settings"
+          v-if="relatedProducts && relatedProducts.length" ref="sliderEl" :arrows="true"
+        >
+            <!-- <div>1</div> -->
+            <div v-for="item in relatedProducts">
+                <Card :product="item"></Card>
+            </div>
+
+          /> 
+          <!-- <div v-for="item in 3">
+            asd
+          </div> -->
+        </vue-slick-carousel>
   </div>
 </template>
  
 <script>
 
   import VueSlickCarousel from 'vue-slick-carousel'
+    import 'vue-slick-carousel/dist/vue-slick-carousel.css';
   import Card from '@/components/Card'
-  import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+//   import 'vue-slick-carousel/dist/vue-slick-carousel.css'
   // optional style for arrows & dots
-  import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
+
+//   import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
  
   export default {
     name: 'MyComponent',
@@ -35,12 +35,12 @@
             settings: {
                 "arrows": true,
                 "dots": false,
-                // "infinite": true,
-                // "centerMode": true,
-                // "centerPadding": "20px",
-                // "slidesToShow": 2,
-                // "slidesToScroll": 1,
-                // "variableWidth": true
+                "infinite": true,
+                "centerMode": true,
+                "centerPadding": "20px",
+                "slidesToShow": 2,
+                "slidesToScroll": 1,
+                "variableWidth": true
             }
         }
     },
